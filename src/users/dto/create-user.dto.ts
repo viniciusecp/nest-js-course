193864de13cbdd0e3f8a23c13c4f0b854e272716
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2025-09-19 22:10:08 -0300

    Created other routes for users

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsEmail()
+  email: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findOne(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+      omit: { passwordHash: true },
+    });
+
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    return user;
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          name: createUserDto.name,
+          email: createUserDto.email,
+          passwordHash: createUserDto.password,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      console.error(error);
+      throw new InternalServerErrorException('Error on user registration');
+    }
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.prisma.user.findFirst({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    try {
+      return this.prisma.user.update({
+        where: { id },
+        data: {
+          name: updateUserDto.name,
+          passwordHash: updateUserDto.password,
+        },
+        omit: { passwordHash: true },
+      });
+    } catch (error) {
+      console.error(error);
+      throw new InternalServerErrorException('Error on user update');
+    }
+  }
+
+  async delete(id: number) {
+    const user = await this.prisma.user.findFirst({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    try {
+      return this.prisma.user.delete({
+        where: { id },
+        omit: { passwordHash: true },
+      });
+    } catch (error) {
+      console.error(error);
+      throw new InternalServerErrorException('Error on user update');
+    }
+  }
+}
