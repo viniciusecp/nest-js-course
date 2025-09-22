fe4e0d8f8644a1b012e97b24560b3c62ff558e55
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2025-09-22 20:37:51 -0300

    Add filters

---
+++ src/common/filters/exception.filter.ts
@@ -0,0 +1,19 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class ApiExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    response.status(exception.getStatus()).json({
+      message: exception.getResponse() || 'Erro ao realizar essa operação.',
+    });
+  }
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { APP_FILTER } from '@nestjs/core';
+import { ApiExceptionFilter } from 'src/common/filters/exception.filter';
 
 @Module({
   imports: [PrismaModule],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [
+    TasksService,
+    {
+      provide: APP_FILTER,
+      useClass: ApiExceptionFilter,
+    },
+  ],
 })
 export class TasksModule {}
