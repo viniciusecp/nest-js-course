79e98655daba023d1fa26b23613062cce0defc3d
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2025-09-27 18:51:07 -0300

    Create user test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,6 +2,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersService } from './users.service';
 import { HashingServiceProtocol } from 'src/auth/hash/hashing.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UsersService', () => {
   let usersService: UsersService;
@@ -14,11 +15,17 @@ describe('UsersService', () => {
         UsersService,
         {
           provide: PrismaService,
-          useValue: {},
+          useValue: {
+            user: {
+              create: jest.fn(),
+            },
+          },
         },
         {
           provide: HashingServiceProtocol,
-          useValue: {},
+          useValue: {
+            hash: jest.fn(),
+          },
         },
       ],
     }).compile();
@@ -29,7 +36,54 @@ describe('UsersService', () => {
   });
 
   it('should be defined users service', () => {
-    console.log('usersService', usersService);
     expect(usersService).toBeDefined();
+  });
+
+  it('should create a new user', async () => {
+    const createUserDto: CreateUserDto = {
+      name: 'John',
+      email: 'john@doe.com',
+      password: '123456',
+    };
+
+    const encryptedPassword = 'ENCRYPTED_PASSWORD';
+
+    const createdUserMock = {
+      id: 1,
+      name: createUserDto.name,
+      email: createUserDto.email,
+      passwordHash: encryptedPassword,
+      active: true,
+      avatar: null,
+      createdAt: new Date(),
+    };
+
+    const hashSpy = jest
+      .spyOn(hashingService, 'hash')
+      .mockResolvedValue(encryptedPassword);
+
+    const createUserSpy = jest
+      .spyOn(prismaService.user, 'create')
+      .mockResolvedValue(createdUserMock);
+
+    const createdUser = await usersService.create(createUserDto);
+    console.log('createdUser', createdUser);
+
+    expect(hashSpy).toHaveBeenCalledWith(createUserDto.password);
+
+    expect(createUserSpy).toHaveBeenCalledWith({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        passwordHash: encryptedPassword,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    expect(createdUser).toEqual(createdUserMock);
   });
 });
