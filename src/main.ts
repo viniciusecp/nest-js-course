190421f04281b223899d1b1368807d777184b760
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2026-01-02 11:21:33 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
